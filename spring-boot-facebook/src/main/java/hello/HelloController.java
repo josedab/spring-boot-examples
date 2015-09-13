c7cd30e1f3a31a999f33b70593376a13844b4b1e
Jose Baena <jose@brandwatch.com>
2015-09-13 15:40:29 +0100

    IS-12 Spring social: facebook showcase - Login capability - Still have problems with permissions, need to get my head around the basic setup. Also, contemplated the idea of using passport and then just storing the information through an endpoint.(Idea)

---
+++ spring-boot-facebook/src/main/java/hello/HelloController.java
@@ -0,0 +1,39 @@
+package hello;
+
+import javax.inject.Inject;
+
+import org.springframework.social.facebook.api.Checkin;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.Page;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    private Facebook facebook;
+
+    @Inject
+    public HelloController(Facebook facebook) {
+        this.facebook = facebook;
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String helloFacebook(Model model) {
+        if (!facebook.isAuthorized()) {
+            return "redirect:/connect/facebook";
+        }
+
+        model.addAttribute(facebook.userOperations().getUserProfile());
+        PagedList<Checkin> homeFeed = facebook.placesOperations().getCheckins();
+        model.addAttribute("feed", homeFeed);
+
+        return "hello";
+    }
+
+}
