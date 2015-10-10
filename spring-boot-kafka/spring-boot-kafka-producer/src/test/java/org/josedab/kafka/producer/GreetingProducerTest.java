8095263d09b265a43ba0877e13bdeb2d56223ac7
Jose Baena <jose@brandwatch.com>
2015-10-10 01:55:18 +0100

    Updated tests for not component scaning when running the tests

--- spring-boot-kafka/spring-boot-kafka-producer/src/test/java/org/josedab/kafka/producer/GreetingProducerTest.java
+++ spring-boot-kafka/spring-boot-kafka-producer/src/test/java/org/josedab/kafka/producer/GreetingProducerTest.java
@@ -5,8 +5,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
+import java.util.concurrent.Future;
+
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.josedab.kafka.domain.Greeting;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,7 +29,10 @@ public class GreetingProducerTest {
     private static final String GREETING_SERIALIZED_BODY = "{\"id\":1444428532713,\"content\":\"Current timestamp for the greeting is 1444428532713\"}";
     
     @Mock
-    private Greeting greeting; 
+    private Greeting greeting;
+    
+    @Mock
+    private Future<RecordMetadata> futureGreetingResult; 
     
     @Mock
     private ObjectMapper objectMapper;
@@ -44,6 +50,7 @@ public class GreetingProducerTest {
     
     @Test
     public void givenGreetingSent_whenSerializedObject_sendsMessageToKafka() throws JsonProcessingException {
+        when(producer.send(any(ProducerRecord.class))).thenReturn(futureGreetingResult);
         when(objectMapper.writeValueAsString(greeting)).thenReturn(GREETING_SERIALIZED_BODY);
         
         greetingProducer.sendGreetingToKafka(greeting);
